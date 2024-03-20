a11d6df4c053065fd92ed099b721ebccac8fa8bb
Paxlavon2206 <paxlavon2206@gmail.com>
2024-03-20 06:15:30 +0500

    dars-7-oy7

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { AppLayout } from "./layout/app-layout"
+import appRouter from "./routes/app-router"
+import { Route, Routes } from "react-router-dom"
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   
+    <Routes>
+      <Route path="/" element={<AppLayout/>}>
+        {appRouter.map(({component,id,path}) => (
+        <Route path={path} element={component} key={id} index={path ? false : true}/>
+        ))}
+      </Route>
+    </Routes>
   )
 }
 
